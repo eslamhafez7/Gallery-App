b1ada9017a328467cfff3a9c14f74f869b339165
Islam Hafez <aboeshaeslam2@gmail.com>
2023-05-15 02:26:55 +0300

    My gallery app

---
+++ javascript/script.js
@@ -0,0 +1,34 @@
+let mainImg = document.getElementById("main-img");
+let subImg = document.querySelectorAll(".image img");
+let nextBtn = document.getElementById("next");
+let prevBtn = document.getElementById("prev");
+
+let currentIndex = 0;
+
+subImg.forEach((subImg, index)=> {
+    subImg.addEventListener('click', () => {
+        mainImg.src = subImg.src;
+        currentIndex = index;
+    });
+});
+
+mainImg.addEventListener('load', () => {
+    mainImg.classList.add("loaded");
+})
+nextBtn.addEventListener('click', () => {
+    currentIndex++;
+    if(currentIndex >= subImg.length) {
+        currentIndex = 0;
+    }
+    mainImg.classList.remove('loaded');
+    mainImg.src = subImg[currentIndex].src;
+});
+
+prevBtn.addEventListener("click", () => {
+    currentIndex--;
+    if (currentIndex < 0) {
+    currentIndex = subImg.length - 1;
+    }
+    mainImg.classList.remove('loaded');
+    mainImg.src = subImg[currentImgIndex].src;
+});
